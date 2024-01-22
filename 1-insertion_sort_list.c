4b4f4a9e4034ac533656b84280b19a51a394253a
MalekAjil <aboanasajil@gmail.com>
2024-01-22 11:43:58 -0800

    task1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,15 @@
+#include "sort.h"
+
+/**
+ * insertion_sort - sorts a doubly linked list of integers in ascending order
+ * using the Insertion sort algorithm
+ *
+ * @list: The list to be sorted
+ */
+void insertion_sort_list(listint_t **list)
+{
+	int i = 0;
+	while (list)
+	{
+	}
+}
