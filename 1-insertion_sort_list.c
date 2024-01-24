eea847b36bd2f61692a52887025e94f4723c8781
MalekAjil <aboanasajil@gmail.com>
2024-01-23 18:50:06 -0800

    task1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,5 +1,17 @@
 #include "sort.h"
 
+void swap(listint_t *A, listint_t *B)
+{
+	A->next = B->next;
+	if (B->next)
+		B->next->prev = A;
+	B->prev = A->prev;
+	B->next = A;
+	if (A->prev)
+		A->prev->next = B;
+	A->prev = B;
+}
+
 /**
  * insertion_sort_list - sorts a doubly linked list of integers in
  * ascending order using the Insertion sort algorithm
@@ -19,17 +31,10 @@ void insertion_sort_list(listint_t **list)
 		cur = cur->next;
 		while (A && A->n > B->n)
 		{
-			A->next = B->next;
-			if (B->next)
-				B->next->prev = A;
-			B->prev = A->prev;
-			B->next = A;
-			if (A->prev)
-				A->prev->next = B;
-			else
-				*list = A;
-			A->prev = B;
-			A = B;
+			swap(A, B);
+			if (!B->prev)
+				*list = B;
+			A = B->prev;
 			print_list(*list);
 		}
 	}
